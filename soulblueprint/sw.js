c821aa5de5ae3aa8c77e9c2dbb3b8f193be9739a
quantummerlin <quantummerlin@users.noreply.github.com>
2026-02-01 09:42:41 +1100

    Fix color scheme inheritance for shared readings

--- soulblueprint/sw.js
+++ soulblueprint/sw.js
@@ -1,5 +1,5 @@
 // A Moment in Time - Service Worker
-const CACHE_NAME = 'moment-in-time-v28';
+const CACHE_NAME = 'moment-in-time-v29';
 const ASSETS_TO_CACHE = [
     '/soulblueprint/',
     '/soulblueprint/index.html',
