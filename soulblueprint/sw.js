aff17fa380080aa0f5cb36786902a030141ba3ce
quantummerlin <quantummerlin@users.noreply.github.com>
2026-01-31 12:42:52 +1100

    Fix: persist gender theme, default sections to collapsed

--- soulblueprint/sw.js
+++ soulblueprint/sw.js
@@ -1,5 +1,5 @@
 // A Moment in Time - Service Worker
-const CACHE_NAME = 'moment-in-time-v11';
+const CACHE_NAME = 'moment-in-time-v12';
 const ASSETS_TO_CACHE = [
     '/soulblueprint/',
     '/soulblueprint/index.html',
