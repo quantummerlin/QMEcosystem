04d39bdff4b459b40c788fd9dae44702897118b7
quantummerlin <quantummerlin@users.noreply.github.com>
2026-01-31 11:56:54 +1100

    Make all sections start collapsed with uniform headers

--- soulblueprint/sw.js
+++ soulblueprint/sw.js
@@ -1,5 +1,5 @@
 // A Moment in Time - Service Worker
-const CACHE_NAME = 'moment-in-time-v9';
+const CACHE_NAME = 'moment-in-time-v10';
 const ASSETS_TO_CACHE = [
     '/soulblueprint/',
     '/soulblueprint/index.html',
