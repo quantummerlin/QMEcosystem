0f1189301e395493406d431d312fa229775c1bc8
quantummerlin <quantummerlin@users.noreply.github.com>
2026-02-19 07:34:00 +1100

    Remove nav bar, simplify welcome to single step, cache bust v47

--- soulblueprint/sw.js
+++ soulblueprint/sw.js
@@ -1,5 +1,5 @@
 // A Moment in Time - Service Worker
-const CACHE_NAME = 'moment-in-time-v46';
+const CACHE_NAME = 'moment-in-time-v47';
 const ASSETS_TO_CACHE = [
     '/soulblueprint/',
     '/soulblueprint/index.html',
