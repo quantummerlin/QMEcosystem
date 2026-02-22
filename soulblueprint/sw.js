34133cec08abcd7c452b6182014607f9a9ac4e24
quantummerlin <quantummerlin@users.noreply.github.com>
2026-02-23 07:35:21 +1100

    Nuclear SW cache bust: unregister SW + purge caches on fulfill pages, skip SW re-registration, bump all scripts to v55

--- soulblueprint/sw.js
+++ soulblueprint/sw.js
@@ -1,5 +1,5 @@
 // A Moment in Time - Service Worker
-const CACHE_NAME = 'moment-in-time-v54';
+const CACHE_NAME = 'moment-in-time-v55';
 const ASSETS_TO_CACHE = [
     '/soulblueprint/',
     '/soulblueprint/index.html',
